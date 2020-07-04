29ba395a8bf00155cd49d0f47d59dce7b96a74a7
Muhammad N. Fawi <m.noor.fawi@gmail.com>
2020-07-05 01:24:13 +0400

    Update app.py

--- app.py
+++ app.py
@@ -2,44 +2,34 @@ import flask
 import lzhw
 from time import time
 import pandas as pd
-#from flask import jsonify
-import numpy
+from flask import jsonify, request
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-df = pd.read_excel("german_credit.xlsx")
-comp = lzhw.CompressedDF(df)
+df = pd.read_csv("1500000 Sales Records.csv")
 
-columns = []
-for c in range(len(comp.compressed)):
-    com = comp.compressed[c].compressed
-    if isinstance(com, numpy.ndarray):
-        com = list(com.astype(str))
-    com = list(map(str, list(com)))
-    columns.append(com)
-	
-seqs = []
-for s in range(len(comp.compressed)):
-	seqs.append(comp.compressed[c].sequences)
-	
-comp_dict = {"columns": columns, "Sequences": seqs}
 
 @app.route('/full', methods=['GET'])
 def get_full():
+	col = int(request.args["col"])
 	start = time()
 	try:
-		return df.to_json()
-	finally:
-		print(time() - start)
-	
-	
-@app.route('/compressed', methods=['GET'])
-def get_compressed():
-	start = time()
-	try:
-		return comp_dict
+		return df.iloc[:, col].to_json()
 	finally:
 		print(time() - start)
 
-app.run()
+@app.route('/compressed', methods=['GET'])
+def get_compressed():
+	col = int(request.args["col"])
+	comp = lzhw.CompressedDF(df, selected_cols = [col])
+	comp.compressed[0].compressed = [bin(i)[2:] for i in comp.compressed[0].compressed]
+	try:
+		start = time()
+		return jsonify({"sequences": comp.compressed[0].sequences, "compressed": comp.compressed[0].compressed})
+	finally:
+		print(time() - start)
+
+if __name__ == "__main__":
+	app.run()
